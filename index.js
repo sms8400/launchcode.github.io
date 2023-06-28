8e135bb152783bf3173549e3857c224dd737c806
icr8FreeCode <109628112+icr8FreeCode@users.noreply.github.com>
2023-06-28 21:16:47 +0000

    migration

--- index.js
+++ index.js
@@ -4,5 +4,4 @@
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
 
 
-
 //Use a SetValue to add the items "false", and "-4.6" to the array.  Print the array to confirm the changes.
