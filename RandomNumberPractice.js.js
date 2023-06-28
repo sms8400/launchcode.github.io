094ddb766602883b69ffa8bd6f09facaf004a9b2
icr8FreeCode <109628112+icr8FreeCode@users.noreply.github.com>
2023-06-28 20:11:19 +0000

    Initial commit

---
+++ RandomNumberPractice.js.js
@@ -0,0 +1,14 @@
+for (i=0; i < 20; i++){
+  let num = Math.random();
+  console.log(num);
+}
+
+/* Modify the code above to:
+1) Generate random integers from 1 - 100. Click 'Run' multiple times to make sure 1 and 100 can be generated, but NOT 0.
+
+2) Generate random integers between -5 and 0, but NOT including 0. Click 'Run' until you verify that -5 can be generated but NOT 0.
+
+3) Challenge: Generate random integers from 20 - 30.
+
+Be sure to click 'Run' several times to verify your code generates random numbers properly.
+*/
