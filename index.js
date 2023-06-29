837c1c84058c6b2250e5bd6d6af0df84f1407472
icr8FreeCode <109628112+icr8FreeCode@users.noreply.github.com>
2023-06-29 16:03:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Welcome, new coder. Enter your code on line 2 to print 'Hello, World!' to the console (the panel on the right)
+
+console.log("Hello");
+
+// Next, PLAY! Modify your code on line 2 to try to accomplish the tasks listed in the "Your First Program" page.
